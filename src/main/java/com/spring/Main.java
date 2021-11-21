eb55eb362b1f78ba39e00660e84a6e7d2f21071e
freegastello <freegastello@gmail.com>
2021-11-21 15:44:40 +0300

    211121 - Пока не работающий вариант WEBMVC

--- src/main/java/com/spring/Main.java
+++ src/main/java/com/spring/Main.java
@@ -2,6 +2,7 @@ package com.spring;
 
 import com.spring.config.InitData;
 import com.spring.config.SpringConfig;
+import com.spring.config.WebAppConfig;
 import com.spring.entity.Animals;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 
 public class Main {
-    private static final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+    private static final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebAppConfig.class);
+//    private static final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
     public static void main(String[] args) {
 //        Logger logger = LoggerFactory.getLogger(Main.class);
@@ -21,6 +23,9 @@ public class Main {
 //        logger.trace("hello trace");
 
 //        context.getBean("initData");
+
+        InitData init = new InitData();
+        init.initData();
 
 //        Animals animals = new Animals("Киса", 5);
     }
---
+++ src/main/webapp/pages/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+<%--    script type=”text/javascript” src=”common_js.js”>/script--%>
+</head>
+<body>
+  Hello JSP
+</body>
+</html>
