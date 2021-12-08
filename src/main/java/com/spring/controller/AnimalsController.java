557e470348a70d7bdb1d3777c10c41b060f567df
freegastello <freegastello@gmail.com>
2021-12-08 22:04:16 +0300

    Сработало с thymeleaf

--- src/main/java/com/spring/controller/AnimalsController.java
+++ src/main/java/com/spring/controller/AnimalsController.java
@@ -1,10 +1,14 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 public class AnimalsController {
-
-
-
+    @GetMapping("/")
+    public ModelAndView testjs(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 }
--- src/main/java/com/spring/dao/AnimalsRepository.java
+++ src/main/java/com/spring/dao/AnimalsRepository.java
@@ -1,12 +1,10 @@
 package com.spring.dao;
 
-//import com.spring.entity.Animals;
+import com.spring.entity.Animals;
 //import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 //@Repository
 public interface AnimalsRepository {
-}
-
 //public interface AnimalsRepository extends JpaRepository<Animals, Long> {
-//}
+}
