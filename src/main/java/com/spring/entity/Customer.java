cda26a050a8728d8bbf31e3c96557c7933de124d
freegastello <freegastello@gmail.com>
2022-09-01 23:53:24 +0300

    РАБОТАЕТ ORACLE!!!

--- src/main/java/com/spring/entity/Customer.java
+++ src/main/java/com/spring/entity/Customer.java
@@ -8,14 +8,18 @@ import java.util.UUID;
 @Entity
 @Table(name = "customer")
 public class Customer {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Basic(optional = false)
-@Column(name = "id")
-@NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    @NotNull
     private long id;
+
+//    @Id
+//    @Column(name = "ID")
+//    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+//    @SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
+//    private int id;
 
     @Column(name = "first_name")
     private String firstName;
